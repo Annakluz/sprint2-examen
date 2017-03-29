5ce37fd71d2abc60f4cce14cccdca237ad5ab0b9
Anna karina Luz <anna.luz.duran@gmail.com>
2017-03-29 17:22:39 +0000

    No se por que no me reconoce la funcion

--- trello.js
+++ trello.js
@@ -1,14 +1,15 @@
 function agregarLista(){
-    var nuevoEspacio = document.createElement("div");
-    var nuevaLista = document.createElement("ul");
-    var nombreLista = document.getElementById("nombreLista")
+    var nuevoEspacio = document.createElement("div"); // Div donde estara la lista
+    var nuevaLista = document.createElement("ul"); // Dentro se cre una lista
+    var nombreLista = document.getElementById("nombreLista") // nombre de la lista
     
-    nuevoEspacio.appendChild(nuevaLista)
-    var espacioDeListas = document.getElementById("cajaDeListas");
-    nombreLista.innerHTML = document.body.insertBefore(nuevoEspacio, espacioDeListas
-   );
+    nuevoEspacio.appendChild(nuevaLista) //nueva lista sera hijo de nuevo espacio
+    var espacioDeListas = document.getElementById("cajaListas"); //aqui estaran los div y la ul
+  
+    nuevaLista.innerHTML = nombreLista.value;
+    document.body.insertBefore(nuevoEspacio, espacioDeListas)
     
-};
+}
 function agregarItem(){
-    
+    var 
 }
